a675e8948159f7c0e52650bf1f1c9d499db718da
nekrasov <nekrasov.aleks@mail.ru>
2023-03-11 17:03:15 +0300

    fix requirements.txt

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 
 requirements_filepath = os.path.join(os.path.dirname(__name__), "requirements.txt")
