3d8eeb537b8fc3f10c3ac6e3b5be1d715e25c526
Aleksei Nekrasov <nekrasov.aleks@mail.ru>
2023-06-01 18:46:29 +0300

    Update version setup.py 0.2.2->1.0.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-__version__ = "0.2.2"
+__version__ = "1.0.0"
 
 
 requirements_filepath = os.path.join(os.path.dirname(__name__), "requirements.txt")
