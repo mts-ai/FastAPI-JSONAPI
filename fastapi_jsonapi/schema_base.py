06b426dc0ee31083da360fe096a8bbf106b65ca0
Suren Khorenyan <s.khorenyan@mts.ai>
2023-07-15 09:48:05 +0300

    update RelationshipInfo, freeze, add id_field_name

--- fastapi_jsonapi/schema_base.py
+++ fastapi_jsonapi/schema_base.py
@@ -47,4 +47,8 @@ class RelationshipInfo(BaseModel):
     many: bool = False
     related_view: str = None
     related_view_kwargs: Dict[str, str] = {}
-    resource_id_example: int = 1
+    resource_id_example: str = "1"
+    id_field_name: str = "id"
+
+    class Config:
+        frozen = True
