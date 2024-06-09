5d2ab66203c17f113e235cdc8b90729bae515d48
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-06-09 13:49:16 +0300

    create common helpers (maybe refactor later?)

---
+++ fastapi_jsonapi/common.py
@@ -0,0 +1,22 @@
+from typing import TYPE_CHECKING
+
+from fastapi_jsonapi.types_metadata import (
+    ClientCanSetId,
+    RelationshipInfo,
+    CustomFilterSQL,
+)
+from fastapi_jsonapi.utils.metadata_instance_search import MetadataInstanceSearch
+
+if TYPE_CHECKING:
+    # noinspection PyProtectedMember
+    from pydantic.fields import FieldInfo
+
+search_client_can_set_id = MetadataInstanceSearch(ClientCanSetId)
+search_relationship_info = MetadataInstanceSearch(RelationshipInfo)
+search_custom_filter_sql = MetadataInstanceSearch(CustomFilterSQL)
+
+
+def get_relationship_info_from_field_metadata(
+    field: "FieldInfo",
+) -> RelationshipInfo | None:
+    return search_relationship_info.first(field)
