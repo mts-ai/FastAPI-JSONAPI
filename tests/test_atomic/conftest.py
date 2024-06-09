bb9ec2a808c0f181df9990e41bfd5b87e0c742c1
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-06-09 17:26:36 +0300

    add atomic_operation_actions_as_str fixture

--- tests/test_atomic/conftest.py
+++ tests/test_atomic/conftest.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import Sequence
+
 import pytest
 
 from fastapi_jsonapi.atomic.schemas import AtomicOperationAction
@@ -10,13 +12,22 @@ def allowed_atomic_actions_list() -> list[str]:
     return [op.value for op in AtomicOperationAction]
 
 
-@pytest.fixture()
-def allowed_atomic_actions_as_string(allowed_atomic_actions_list) -> str:
-    if len(allowed_atomic_actions_list) == 1:
-        return repr(allowed_atomic_actions_list[0])
+def options_as_pydantic_choices_string(options: Sequence[str]) -> str:
+    if len(options) == 1:
+        return repr(options[0])
     return " or ".join(
         (
-            ", ".join(repr(op) for op in allowed_atomic_actions_list[:-1]),
-            repr(allowed_atomic_actions_list[-1]),
+            ", ".join(repr(op) for op in options[:-1]),
+            repr(options[-1]),
         )
     )
+
+
+@pytest.fixture()
+def atomic_operation_actions_as_str():
+    return options_as_pydantic_choices_string([v.value for v in AtomicOperationAction])
+
+
+@pytest.fixture()
+def allowed_atomic_actions_as_string(allowed_atomic_actions_list) -> str:
+    return options_as_pydantic_choices_string(allowed_atomic_actions_list)
