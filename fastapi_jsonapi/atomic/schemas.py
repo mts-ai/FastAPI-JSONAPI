364fbdd9d3a1d23f8e8dce58f97cfb05d41332e1
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-06-09 16:04:02 +0300

    update operation item in schema annotations

--- fastapi_jsonapi/atomic/schemas.py
+++ fastapi_jsonapi/atomic/schemas.py
@@ -17,10 +17,10 @@ class OperationItemInSchema(BaseModel):
     """
 
     type: str = Field(default=..., description="Resource type")
-    id: Optional[str] = Field(default=None, description="Resource object ID")
-    lid: Optional[str] = Field(default=None, description="Resource object local ID")
-    attributes: Optional[dict] = Field(None, description="Resource object attributes")
-    relationships: Optional[dict] = Field(None, description="Resource object relationships")
+    id: str | None = Field(default=None, description="Resource object ID")
+    lid: str | None = Field(default=None, description="Resource object local ID")
+    attributes: dict | None = Field(None, description="Resource object attributes")
+    relationships: dict | None = Field(None, description="Resource object relationships")
 
 
 OperationDataType = Union[
