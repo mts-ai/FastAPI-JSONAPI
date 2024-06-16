dda7baa0db3eded1ee1ddae3fc962c83e80d7462
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-06-16 21:42:56 +0300

    create JSONB Contains Filter SQL
    TODO: tests coverage

--- fastapi_jsonapi/contrib/sqla/filters.py
+++ fastapi_jsonapi/contrib/sqla/filters.py
@@ -1,17 +1,32 @@
-from typing import cast
+from typing import (
+    TYPE_CHECKING,
+    cast,
+)
 
-from sqlalchemy import func
+from sqlalchemy import (
+    BinaryExpression,
+    BooleanClauseList,
+    func,
+)
 from sqlalchemy.orm import InstrumentedAttribute
-from sqlalchemy import BinaryExpression
 
 from fastapi_jsonapi.types_metadata import CustomFilterSQL
-from pydantic.fields import FieldInfo
+
+if TYPE_CHECKING:
+    from pydantic.fields import FieldInfo
 
 
-class LowerEqualsFilterSQL(CustomFilterSQL[InstrumentedAttribute, BinaryExpression]):
+SQLAExpressionType = BinaryExpression | BooleanClauseList
+
+
+class CustomFilterSQLA(CustomFilterSQL[InstrumentedAttribute, SQLAExpressionType]):
+    """Base class for custom SQLAlchemy filters"""
+
+
+class LowerEqualsFilterSQL(CustomFilterSQLA):
     def get_expression(
         self,
-        schema_field: FieldInfo,
+        schema_field: "FieldInfo",
         model_column: InstrumentedAttribute,
         value: str,
         operator: str,
@@ -22,4 +37,17 @@ class LowerEqualsFilterSQL(CustomFilterSQL[InstrumentedAttribute, BinaryExpressi
         )
 
 
+# TODO: tests coverage
+class JSONBContainsFilterSQL(CustomFilterSQLA):
+    def get_expression(
+        self,
+        schema_field: "FieldInfo",
+        model_column: InstrumentedAttribute,
+        value: str,
+        operator: str,
+    ) -> BinaryExpression:
+        return model_column.op("@>")(value)
+
+
 sql_filter_lower_equals = LowerEqualsFilterSQL(op="lower_equals")
+sql_filter_jsonb_contains = JSONBContainsFilterSQL(op="jsonb_contains")
